290c3194b1d8b073a724c90db9eb4c60dc2199fb
Emseh <andrianrizkieffendi@gmail.com>
2023-10-22 22:26:31 +0700

    Display messages using partial

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,12 +11,16 @@ class MessagesController < ApplicationController
     return unless message.save
 
     ActionCable.server.broadcast 'chatroom_channel',
-                                 foo: message.body
+                                 mod_message: message_render(message)
   end
 
   private
 
   def message_params
     params.require(:message).permit(:body)
+  end
+
+  def message_render(message)
+    render(partial: 'message', locals: { message: message })
   end
 end
