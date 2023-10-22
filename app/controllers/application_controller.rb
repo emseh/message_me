0d5aa5121e8282e7b98985beaf39d095e59c5d89
Emseh <andrianrizkieffendi@gmail.com>
2023-10-22 20:22:37 +0700

    Add authentication system - create and destroy sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
+# frozen_string_literal: true
+
+# The ApplicationController class in Ruby is a base class for controllers that provides helper methods
+# for managing user authentication and authorization.
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_user
+    return if logged_in?
+
+    flash[:error] = 'You must be logged in to perform that action'
+    redirect_to login_path
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,24 @@
+# frozen_string_literal: true
+
+# The SessionsController class is responsible for handling new session requests.
 class SessionsController < ApplicationController
-  def new
+  def new; end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user&.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = 'You have successfully logged in'
+      redirect_to root_path
+    else
+      flash.now[:error] = 'There was something wrong with your login information'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = 'You have successfully logged out'
+    redirect_to login_path
   end
 end
