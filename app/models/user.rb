f693c4dff98251b696b6e192063c24c37bc08750
Emseh <andrianrizkieffendi@gmail.com>
2023-10-22 18:56:24 +0700

    Build Message resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   validates :username, presence: true, length: { minimum: 3, maximum: 15 },
-                       uniquenees: { case_sensitive: false }
+                       uniqueness: { case_sensitive: false }
+  has_many :messages
 end
